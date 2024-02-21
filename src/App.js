ec774f93c71289a71c8d114a52090b1a5db6cb5d
HusseinAshraf <husseinashraf7414@gmail.com>
2024-02-22 01:03:46 +0200

    initial cot

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RouterProvider , createBrowserRouter } from 'react-router-dom';
+import { RouterProvider , createBrowserRouter, createHashRouter } from 'react-router-dom';
 import About from './component/About/About';
 import Contact from './component/Contact/Contact';
 import Layout from './component/Layout/Layout';
@@ -7,7 +7,7 @@ import Home from './component/Home/Home';
 import Portfolio from './component/Portfolio/Portfolio';
 
 
-let routers = createBrowserRouter([
+let routers = createHashRouter([
   {path :'' , element: <Layout/> ,
    children :[
     {index: true , element: <Home/>},
